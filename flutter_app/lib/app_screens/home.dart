be6d58d590b5ec2104608afd2cb6ac522ceb0cd9
mohamedbrahimi <mohamedaissabrahimi@gmail.com>
2019-09-05 22:47:28 +0100

    added list of screen to the main file.

---
+++ flutter_app/lib/app_screens/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: Container(
+          alignment: Alignment.center,
+          // margin: EdgeInsets.only(left: 35.0, top: 50),
+          color: Colors.cyan,
+          child: Text("YaMan", textDirection: TextDirection.ltr,)
+      ),
+    );
+  }
+}
